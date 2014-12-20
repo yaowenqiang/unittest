aa7cdce2e031cf456764d6b0b1983adfdbf7b71d
yaowenqiang <yaowenqiang111@163.com>
2014-12-20 21:22:43 +0000

    add Annot testcase

---
+++ stats/Test/AnnotTest.php
@@ -0,0 +1,40 @@
+<?php
+    namespace stats\Test;
+    use stats\Baseball;
+    require_once "Baseball.php";
+    class BaseballTest2 extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * @dataProvider providerCaltArgs
+         * @covers Baseball::cal_avg
+         * @return void
+         * @author Steve Francia <steve.francia@gmail.com>
+         */
+        public function testCalc($atbats,$hits)
+        {
+            $baseball = new Baseball();
+            $result = $baseball->calc_avg($atbats,$hits);
+            $expectResult = $hits/$atbats;
+            $formatexpectedresult = number_format($hits/$atbats,3);
+            $this->assertEquals($formatexpectedresult,$result);
+        }
+        /**
+         * undocumented public function ()
+         {
+
+         }
+         *
+         * @return void
+         * @author Steve Francia <steve.francia@gmail.com>
+         */
+        public function providerCaltArgs()
+        {
+            return array(
+                array('389','129'),
+                array('somestring',129),
+                array(389,'somestring'),
+                array('somestring','somestring')
+            );
+        }
+    }
+?>
